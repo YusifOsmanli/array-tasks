dab4d24591ffa9a7e579af0daef6582c0a94ca90
YusifOsmanli <tu6xkjyle@code.edu.com>
2023-10-31 03:28:28 +0400

    First commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// let yusif=[10,20,50,98,101,150,200]
+// let cem3 = 0
+// let digercem = 0
+
+// for( i=0 ;i<yusif.length;i++){
+//     if(yusif[i]/100>1){
+//         cem3+=yusif[i]
+//     }else if(yusif[i]/100<1){
+//         digercem+=yusif[i]
+//     }
+
+
+// } console.log(cem3-digercem);
+  
+
+
+// // 3.Ədədin istənilən qüvvətini tapan alqoritm qurun.User ədəd və qüvvət dəyərlərini prompt vasitəsi ilə daxil edəcək və ekranda daxil etdiyi ədədin
+
+
+//  let a = parseInt(prompt("a ededi ve quvvetini yazin"))
+//  let n =parseInt(prompt( "n ededin daxil edin"))
+//  if(a){
+//     if(n){
+//         alert(a**n)
+//     }
+
+//  }
+
+// // 5.Array-də(minimum 5 element olsun) tək ədədlərlə cüt ədədlərin fərqini tapan alqoritm qurun
+//  let Array = [1,2,3,4,5,6,7,8,9]
+//  let tekcem = 0
+//  let cutcem = 0
+//  for(i=0;i<Array.length; i++){
+//     if(Array[i]%2==0){
+//         cutcem+=Array[i]
+
+//     }else if(Array[i]%2==1){
+//         tekcem+=Array[i]
+//     }
+
+
+//  }console.log(tekcem-cutcem);
+ 
+// .Array-də(minimum 5 element olsun) tərsinə çevirən alqoritm qurun.(misal [1,2,3,4,5]=>[5,4,3,2,1])
+
+// let yusif = [1,2,3,4,5,6,7,8]
+
+// let yusif1 = [ 0,0,0,0,0,0,0,0]
+// for(i=0; i<yusif.length;i++){
+//     for( j=yusif.length ;j>=0; i--){
+//         console.log(yusif1[j]=yusif[i])
+//     } 
+// }
+
+
+
+
+
+ 
+// 7. 2 arrayiniz olsun. Birinci arraydə olub eyni zamanda ikinci arrayda olmayan bütün elementləri ekranda çap edən alqoritm qurun.(Misal [1,2,3,4,5],[1,4,5,6,7]=>[2,3])
+// let yusif1=[1,2,3,4,5]
+// let yusif2=[1,4,5,67.90]
+// let ferqli=[]
+// for(let i=0;i<yusif1.length;i++){
+//     if(!yusif2.includes(yusif1[i])){
+//         eyni.push(yusif1[i])
+//     }
+// }
+// console.log(ferqli)
+ 
+
+ 
+// 2.Daxil edilən ədədin sadə vəya mürəkkəb olub olmamasını yoxlayan alqoritm qurun.Ədəd sadədirsə true əks halda false ekranda çap edilsin.
+
+// 
